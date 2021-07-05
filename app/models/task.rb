ac2b2af3d0a5986aca4846a8441c2465d0d4b9f4
Matt Kragen <mattdkragen@gmail.com>
2021-07-05 12:35:21 -0700

    Write method for #laundry?

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
   # create an instance method
   # which returns 'true' if task title is 'laundry'
+  def laundry?
+    if title == 'laundry'
+      return true
+    else
+      return false
+    end
+  end
 end
