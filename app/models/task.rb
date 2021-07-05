2281394d63f13491a8bb656859ebedb177584418
Matt Kragen <mattdkragen@gmail.com>
2021-07-05 12:30:38 -0700

    Add test for laundry to task_spec

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
-
+  # create an instance method
+  # which returns 'true' if task title is 'laundry'
 end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe 'instance methods' do
+    describe '#laundry?' do
+      it "returns true if task title is 'laundry'" do
+        task = Task.create!(title: 'laundry', description: 'clean the clothes')
+
+        expect(task.laundry).to be(true)
+      end
+    end
+  end
+  describe 'class methods'
+  describe 'relationship'
+  describe 'validations'
+end
